c0e6952c28749c05655ac98a989caafcd522675f
salmaberradia <71037715+salmaberradia@users.noreply.github.com>
2020-11-09 14:12:00 -0500

    exo 1,2,3 terminer

--- exercice.py
+++ exercice.py
@@ -8,18 +8,29 @@ import numpy as np
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.array([])
+
+    return np.linspace(-1.3, 2.5, 64)
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+    a= np.zeros([len(cartesian_coordinates), 2])
+
+    for i in range(len(cartesian_coordinates)):
+        rho = np.sqrt(cartesian_coordinates[i][0] ** 2 + cartesian_coordinates[i][1] ** 2)
+        phi = np.arctan2(cartesian_coordinates[i][1], cartesian_coordinates[i][0])
+        polar_coordinate = (rho, phi)
+        a[i] = polar_coordinate
+
+    return a
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
-    return 0
+    
+    return np.abs(values - number).argmin()
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
-    pass
+    
+   
